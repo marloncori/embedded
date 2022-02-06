37c07294e33b889540ec1663b700a64b438d55ac
Marlon Couto Ribeiro <84450525+marloncori@users.noreply.github.com>
2022-02-06 11:32:41 +0100

    Update atomic_variable.cpp

--- atomic_variable.cpp
+++ atomic_variable.cpp
@@ -3,48 +3,41 @@
 #include <thread>
 #include <vector>
 
+#define INDEX_A 3000
+#define INDEX_B 2000
+#define TIMEOUT_A 1
+#define TIMEOUT_B 2
+
 std::atomic<size_t> sharedIndex{0};
 std::vector<int> data;
 
-class AtomicWorker 
-{
-  private:
-    int index;
-    unsigned long timeout;
-    size_t workerIndex;
-    std::string message;
+size_t workerIndex;
+std::string message;
  
- public:
-    AtomicWorker(int id, unsigned long delay) : index(id), timeout(delay){}
-    
-    void startJob()
-    {
+void startJob()
+{
       while(True){
         workerIndex = sharedIndex.fetch_add(1);
         if(workerIndex >= data.size()){
            break;
         }
-        message = "\033[1;34m---> Worker " + std::to_string(index) + "\033[0m\033[1;35mhandles " + std::to_string(workerIndex) + ".\n\033[0m";
+        message = "\033[1;34m---> Worker " + std::to_string(index) + "\033[0m\033[1;35m handles " + std::to_string(workerIndex) + ".\n\033[0m";
         std::cout << message << std::endl;
         data[workerIndex] = data[workerIndex] * 2;
         std::this_thread::sleep_for(std::chrono::milliseconds(timeout));
       }
-    }
-};
-  
+}
+
 int main()
 {
   int counter = 0;
   for(int i=0; i<10; i++){
      data.emplace_back(i);
   }
-  
-  AtomicWorker w1(1, 3000);
-  AtomicWorker w2(2, 2000);
-    
+      
   std::vector<std::thread> atomicThreads;
-  atomicThreads.emplace_back(std::thread(w1.startJob, w1.index, w1.timeout));
-  atomicThreads.emplace_back(std::thread(w2.startJob, w2.index, w2.timeout));
+  atomicThreads.emplace_back(std::thread(startJob, INDEX_A, TIMEOUT_A));
+  atomicThreads.emplace_back(std::thread(startJob, INDEX_B, TIMEOUT_B));
   
   for(auto& thread : atomicThreads)
   {
@@ -62,7 +55,7 @@ int main()
     std::this_thread::sleep_for(std::chrono::milliseconds(1000));
   }
   
-  std::cout << "\n ------ Thread application has successfully finished. ------- " << std::endl;
+  std::cout << "\n ---- Thread application has successfully finished. ---- " << std::endl;
   
   return 0;
 }
